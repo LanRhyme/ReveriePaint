edc3e7f3678bc76df890ad43efd38bcf27a388af
LanRhyme <xiao_ren233@foxmail.com>
2026-08-15 14:13:59 +0800

    fix: 使用 KisTransparencyMask 蒙版机制实现精准高帧率剪切蒙版与基底笔画联动

--- app/src/main/cpp/ReverieCore.cpp
+++ app/src/main/cpp/ReverieCore.cpp
@@ -53,11 +53,25 @@
 #endif
 #include <algorithm>
 #include <queue>
+#include <kis_transparency_mask.h>
 #include <kis_fill_painter.h>
 #include <kis_gradient_painter.h>
 #include <kis_transform_worker.h>
 #include <kis_warptransform_worker.h>
 #include <kis_perspectivetransform_worker.h>
+
+static KisTransparencyMaskSP findClippingMask(KisNode *node)
+{
+    if (!node) return nullptr;
+    KisNodeSP child = node->firstChild();
+    while (child) {
+        if (KisTransparencyMask *mask = dynamic_cast<KisTransparencyMask *>(child.data())) {
+            return KisTransparencyMaskSP(mask);
+        }
+        child = child->nextSibling();
+    }
+    return nullptr;
+}
 #include <kis_default_bounds.h>
 #include <KoColor.h>
 #include <QPainter>
@@ -1214,8 +1228,8 @@ bool ReverieCore::layerClipped(int index) const
     if (index < 0 || index >= m_layers.size()) {
         return false;
     }
-    if (KisLayer *layer = dynamic_cast<KisLayer *>(m_layers[index].node)) {
-        return layer->alphaChannelDisabled();
+    if (m_layers[index].node && findClippingMask(m_layers[index].node)) {
+        return true;
     }
     return m_layers[index].clipped;
 }
@@ -1226,8 +1240,36 @@ void ReverieCore::setLayerClipped(int index, bool clipped)
         return;
     }
     m_layers[index].clipped = clipped;
-    if (KisLayer *layer = dynamic_cast<KisLayer *>(m_layers[index].node)) {
-        layer->disableAlphaChannel(clipped);
+    KisNode *node = m_layers[index].node;
+    if (!node || !m_document) {
+        return;
+    }
+
+    KisTransparencyMaskSP existingMask = findClippingMask(node);
+
+    if (clipped) {
+        KisPaintDeviceSP baseDev;
+        for (int i = index - 1; i >= 0; --i) {
+            if (!m_layers[i].clipped) {
+                baseDev = layerPaintDeviceFor(m_layers[i]);
+                if (baseDev) break;
+            }
+        }
+        if (baseDev) {
+            KisLayer *parentLayer = dynamic_cast<KisLayer *>(node);
+            if (!existingMask) {
+                existingMask = new KisTransparencyMask(m_document, QStringLiteral("ClippingMask"));
+                existingMask->initSelection(baseDev, parentLayer);
+                m_document->addNode(existingMask, KisNodeSP(node));
+            } else {
+                existingMask->initSelection(baseDev, parentLayer);
+                existingMask->setDirty();
+            }
+        }
+    } else {
+        if (existingMask) {
+            m_document->removeNode(existingMask);
+        }
     }
     recompositeProjection();
     markDirty();
@@ -2679,6 +2721,18 @@ void ReverieCore::touchStrokeEnd()
         m_strokeTxn = nullptr;
         m_strokeTxnActive = false;
         m_redoCount = 0;
+
+        // If any layers above are clipped to the active layer, refresh their masks
+        for (int i = m_currentLayer + 1; i < m_layers.size(); ++i) {
+            if (!m_layers[i].clipped) break;
+            KisPaintDeviceSP baseDev = layerPaintDeviceFor(m_layers[m_currentLayer]);
+            if (baseDev) {
+                if (KisTransparencyMaskSP mask = findClippingMask(m_layers[i].node)) {
+                    mask->initSelection(baseDev, dynamic_cast<KisLayer *>(m_layers[i].node));
+                    mask->setDirty();
+                }
+            }
+        }
     }
     m_drawing = false;
 }
