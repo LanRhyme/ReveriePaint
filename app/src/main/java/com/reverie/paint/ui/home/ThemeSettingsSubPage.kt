778c26affe2574e70f6a4e0bd63cc328fc63e9e8
LanRhyme <xiao_ren233@foxmail.com>
2026-08-25 15:09:46 +0800

    chore(ui): 清理 Switch/TextButton 等失效的 material3 导入

--- app/src/main/java/com/reverie/paint/ui/home/ThemeSettingsSubPage.kt
+++ app/src/main/java/com/reverie/paint/ui/home/ThemeSettingsSubPage.kt
@@ -46,8 +46,6 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
-import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
