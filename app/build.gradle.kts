052a166f950c0d9a6fb573c9a4c23b1768e6bc55
LanRhyme <xiao_ren233@foxmail.com>
2026-09-04 23:32:45 +0800

    chore(release): 发布 v1.0.0-Preview7 版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.reverie.paint"
         minSdk = 23
         targetSdk = 33
-        versionCode = 7
-        versionName = "1.0.0-Preview6"
+        versionCode = 8
+        versionName = "1.0.0-Preview7"
 
         buildConfigField("String", "AIFADIAN_API_TOKEN", "\"$aifadianApiToken\"")
         buildConfigField("String", "AIFADIAN_USER_ID", "\"$aifadianUserId\"")
