14c0831ee30284262b95e6b95f47d31706b915ee
LanRhyme <xiao_ren233@foxmail.com>
2026-09-16 20:32:05 +0800

    chore(release): bump version to 1.0.3 (code 12)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         }
         minSdk = 23
         targetSdk = 33
-        versionCode = 11
-        versionName = "1.0.2"
+        versionCode = 12
+        versionName = "1.0.3"
 
         buildConfigField("String", "AIFADIAN_API_TOKEN", "\"$aifadianApiToken\"")
         buildConfigField("String", "AIFADIAN_USER_ID", "\"$aifadianUserId\"")
