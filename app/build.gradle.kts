41ce64516aff7add4ec81115175578e13a46cf55
LanRhyme <xiao_ren233@foxmail.com>
2026-09-26 23:03:30 +0800

    chore(release): 发布 1.3.3 版本并更新变更日志

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
         }
         minSdk = 23
         targetSdk = 33
-        versionCode = 19
-        versionName = "1.3.2"
+        versionCode = 20
+        versionName = "1.3.3"
 
         buildConfigField("String", "AIFADIAN_API_TOKEN", "\"$aifadianApiToken\"")
         buildConfigField("String", "AIFADIAN_USER_ID", "\"$aifadianUserId\"")
